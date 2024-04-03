5932d447a99037e1b67f72b1fb06c20de3689482
rrekaf <pewuka2005@wp.pl>
2024-04-03 15:35:39 +0200

    commiting now that it works before i break it again

--- main.go
+++ main.go
@@ -28,29 +28,6 @@ type ztmResponse struct {
 	LastUpdate string
 	Vehicles   []ztmVehicle
 }
-
-func fetchData() ztmResponse {
-	resp, err := http.Get("https://ckan2.multimediagdansk.pl/gpsPositions?v=2")
-	if err != nil {
-		log.Fatal("Error fetching JSON: ", err)
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Error reading ztmResponse body: ", err)
-	}
-
-	var data ztmResponse
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		log.Fatal("Error unmarshaling json data: ", err)
-	}
-
-	prepBuses(data)
-	return data
-}
-
 type vehicle struct {
 	Generated              string
 	TripId                 int
@@ -68,13 +45,33 @@ type vehicle struct {
 	GpsQuality             int
 }
 
-// TODO
-func prepBuses(data ztmResponse) map[string][]vehicle {
-	var vehicles map[string][]vehicle
+func fetchData() (map[string][]vehicle, string) {
+	resp, err := http.Get("https://ckan2.multimediagdansk.pl/gpsPositions?v=2")
+	if err != nil {
+		log.Fatal("Error fetching JSON: ", err)
+	}
 
-	// I don't like this but i want to have users choose a vehicle from a dropdown list
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading ztmResponse body: ", err)
+	}
+
+	var data ztmResponse
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal("Error unmarshaling json data: ", err)
+	}
+
+	// Format the ztm api response the way i want
+	return prepBuses(data), data.LastUpdate
+}
+func prepBuses(data ztmResponse) map[string][]vehicle {
+	vehicles := make(map[string][]vehicle)
+
+	// I don't like this but i want to have users choose a vehicle from a dropdown list.
 	// route short name is the most important because that's what it says at bus and tram stops
-	// e.g. '174' -> [ {}, {}, {} ]
+	// e.g. '174' -> [ {"data": data}, {"data": data}, {"data": data} ]
 
 	for _, b := range data.Vehicles {
 		vehicles[b.RouteShortName] = append(vehicles[b.RouteShortName], vehicle{
@@ -98,8 +95,27 @@ func prepBuses(data ztmResponse) map[string][]vehicle {
 }
 
 func main() {
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
+	data, updatedAt := fetchData()
+	http.HandleFunc("/api/vehicles", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Fatal("Error marshaling data: ", err)
+		}
+		w.Write(jsonData)
+
+	})
+
+	http.HandleFunc("/api/updated", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		jsonData, err := json.Marshal(updatedAt)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Fatal("Error marshaling data: ", err)
+		}
+		w.Write(jsonData)
+
 	})
 
 	fetchData()
